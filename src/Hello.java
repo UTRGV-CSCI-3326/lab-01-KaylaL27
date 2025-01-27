09fd3939af8b1d9b952321b7529c2927f8e553a5
Kayla <144965767+KaylaL27@users.noreply.github.com>
2025-01-27 21:09:06 +0000

    Completed the lab

--- src/Hello.java
+++ src/Hello.java
@@ -1,1 +1,10 @@
 // Hello World
+
+//created public class with same name as file
+public class Hello {
+    //created the main method
+    public static void main(String[] args){
+        //now, we output with the new endl
+        System.out.println("Hello World!");
+    }
+}
